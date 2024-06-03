d9abaadccae37fc9a66968fa5afe3b81f6e00161
94515kevin <150640275+94515kevin@users.noreply.github.com>
2024-06-04 07:01:34 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,108 @@
+/* MoveNet Skeleton - Steve's Makerspace (most of this code is from TensorFlow)
+
+MoveNet is developed by TensorFlow:
+https://www.tensorflow.org/hub/tutorials/movenet
+
+*/
+
+let video, bodypose, pose, keypoint, detector;
+let poses = [];
+
+function preload(){
+  horseImg = loadImage("upload_bc549284c3544930bf04fef1eb154c5d.gif");
+}
+
+async function init() {
+  const detectorConfig = {
+    modelType: poseDetection.movenet.modelType.MULTIPOSE_LIGHTNING,
+  };
+  detector = await poseDetection.createDetector(
+    poseDetection.SupportedModels.MoveNet,
+    detectorConfig
+  );
+}
+
+async function videoReady() {
+  console.log("video ready");
+  await getPoses();
+}
+
+async function getPoses() {
+  if (detector) {
+    poses = await detector.estimatePoses(video.elt, {
+      maxPoses: 2,
+      //flipHorizontal: true,
+    });
+  }
+  requestAnimationFrame(getPoses);
+}
+
+async function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO, videoReady);
+  video.size(width, height);
+  video.hide();
+  await init();
+
+  stroke(255);
+  strokeWeight(5);
+}
+
+function draw() {
+  image(video, 0, 0);
+  drawSkeleton();
+  // flip horizontal
+  cam = get();
+  translate(cam.width, 0);
+  scale(-1, 1);
+  image(cam, 0, 0);
+}
+
+function drawSkeleton() {
+  // Draw all the tracked landmark points
+  for (let i = 0; i < poses.length; i++) {
+    pose = poses[i];
+    // eye
+    partL = pose.keypoints[1];
+    partR = pose.keypoints[2];
+    
+      if( partL.score> 0.1) {
+       image(horseImg,partL.x-25,partL.y-25,50,50)
+      }
+    
+      if( partR.score> 0.1) {
+        image(horseImg,partR.x-25,partR.y-25,50,50)
+       }
+    // hand
+    partA = pose.keypoints[0];
+    
+    if (partA.score > 0.1 ) {
+      push()
+      textSize(40)
+      scale(1,1)
+      text("412730300洪子翔",partA.x-width,partA.y-100)
+      
+    }
+    
+  }
+}
+
+/* Points (view on left of screen = left part - when mirrored)
+  0 nose
+  1 left eye
+  2 right eye
+  3 left ear
+  4 right ear
+  5 left shoulder
+  6 right shoulder
+  7 left elbow
+  8 right elbow
+  9 left wrist
+  10 right wrist
+  11 left hip
+  12 right hip
+  13 left kneee
+  14 right knee
+  15 left foot
+  16 right foot
+*/
