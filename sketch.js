82c3b7f48815f6db140ae6dbed54d5427b769dbf
94515kevin <150640275+94515kevin@users.noreply.github.com>
2024-06-04 07:09:45 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,6 @@
-/* MoveNet Skeleton - Steve's Makerspace (most of this code is from TensorFlow)
-
-MoveNet is developed by TensorFlow:
-https://www.tensorflow.org/hub/tutorials/movenet
-
-*/
-
-let video, bodypose, pose, keypoint, detector;
+let video, detector;
 let poses = [];
+let horseImg;
 
 function preload(){
   horseImg = loadImage("upload_bc549284c3544930bf04fef1eb154c5d.gif");
@@ -31,18 +25,19 @@ async function getPoses() {
   if (detector) {
     poses = await detector.estimatePoses(video.elt, {
       maxPoses: 2,
-      //flipHorizontal: true,
     });
   }
   requestAnimationFrame(getPoses);
 }
 
-async function setup() {
+function setup() {
   createCanvas(640, 480);
   video = createCapture(VIDEO, videoReady);
   video.size(width, height);
   video.hide();
-  await init();
+  init().then(() => {
+    console.log("Detector initialized");
+  });
 
   stroke(255);
   strokeWeight(5);
@@ -50,40 +45,41 @@ async function setup() {
 
 function draw() {
   image(video, 0, 0);
-  drawSkeleton();
+
   // flip horizontal
-  cam = get();
-  translate(cam.width, 0);
+  push();
+  translate(width, 0);
   scale(-1, 1);
-  image(cam, 0, 0);
+  image(video, 0, 0);
+  drawSkeleton();
+  pop();
 }
 
 function drawSkeleton() {
   // Draw all the tracked landmark points
   for (let i = 0; i < poses.length; i++) {
-    pose = poses[i];
+    const pose = poses[i];
     // eye
-    partL = pose.keypoints[1];
-    partR = pose.keypoints[2];
+    const partL = pose.keypoints[1];
+    const partR = pose.keypoints[2];
     
-      if( partL.score> 0.1) {
-       image(horseImg,partL.x-25,partL.y-25,50,50)
-      }
-    
-      if( partR.score> 0.1) {
-        image(horseImg,partR.x-25,partR.y-25,50,50)
-       }
-    // hand
-    partA = pose.keypoints[0];
-    
-    if (partA.score > 0.1 ) {
-      push()
-      textSize(40)
-      scale(1,1)
-      text("412730300洪子翔",partA.x-width,partA.y-100)
-      
+    if (partL.score > 0.1) {
+      image(horseImg, partL.x - 25, partL.y - 25, 50, 50);
     }
     
+    if (partR.score > 0.1) {
+      image(horseImg, partR.x - 25, partR.y - 25, 50, 50);
+    }
+    
+    // hand (left wrist)
+    const partA = pose.keypoints[9];
+    
+    if (partA.score > 0.1) {
+      push();
+      textSize(40);
+      text("412730300洪子翔", partA.x, partA.y - 100);
+      pop();
+    }
   }
 }
 
@@ -101,7 +97,7 @@ function drawSkeleton() {
   10 right wrist
   11 left hip
   12 right hip
-  13 left kneee
+  13 left knee
   14 right knee
   15 left foot
   16 right foot
